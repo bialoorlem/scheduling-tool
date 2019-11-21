e101aae247466810c4835b33ee36c12e89ef49fe
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-21 10:24:36 -0600

    Updated IDs for inputs and defined them in JS.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ showCurrentDate();
 //Saving in local storage, received help from Jackson and Bryan
 
 
+
   document.querySelectorAll(".btn").forEach(function(button){
     button.addEventListener("click", function(){
     saveInput();  
@@ -27,3 +28,32 @@ showCurrentDate();
     localStorage.setItem("userInput", userInput);
   }
 
+let savedValue = localStorage.getItem("userInput");
+
+let userInput1 = document.getElementById("userInput1");
+
+let userInput2 = document.getElementById("userInput2");
+
+let userInput3 = document.getElementById("userInput3");
+
+let userInput4 = document.getElementById("userInput4");
+
+let userInput5 = document.getElementById("userInput5");
+
+let userInput6 = document.getElementById("userInput6");
+
+let userInput7 = document.getElementById("userInput7");
+
+let userInput8 = document.getElementById("userInput8");
+
+let userInput9 = document.getElementById("userInput9");
+
+let userInput0 = document.getElementById("userInput0");
+
+
+
+function pullInput(){
+
+userInput.innerHTML = savedValue
+
+}
