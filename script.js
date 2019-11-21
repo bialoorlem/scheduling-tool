2991a2452851eaa45a3db7f59f13f6037a51de53
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-20 20:47:51 -0600

    Added reference to MDN for Local Storage, working on that more tomorrow.

--- script.js
+++ script.js
@@ -9,13 +9,22 @@ function showCurrentDate() {
 }
 showCurrentDate();
 
+//Local Storage help from: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+localStorage.setItem('myCat', 'Tom');
+
+
 //Saving in local storage
+
+
 
 function saveInput() {
 
+ const save = document.getElementById("save");
+ save.addEventListener("click", saveInput);
 
-  let inputEl = document.getElementById("save");
   let savedValue = localStorage.getItem("userInput");
+  console.log(worked)
   
 }
   saveInput();
