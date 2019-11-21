7e2d0367c48dca54586a2d276ab40dd2f6a2b2ff
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-20 20:13:00 -0600

    Added IDs to save buttons, cleaned up JS function. Still working on getting buttons to save to local storage.

--- script.js
+++ script.js
@@ -9,41 +9,13 @@ function showCurrentDate() {
 }
 showCurrentDate();
 
+//Saving in local storage
+
 function saveInput() {
 
 
-  const clockEl = document.getElementById("clock");
-  let savedValue = localStorage.getItem("time");
+  let inputEl = document.getElementById("save");
+  let savedValue = localStorage.getItem("userInput");
   
-  
-  let myInterval = null;
-
-    if (myInterval) {
-      clearInterval(myInterval);
-      myInterval = null;
-    }
-    myInterval = setInterval(function() {
-      time = time - 1;
-
-      let hours = Math.floor(time / 60 / 60);
-      let minutes = Math.floor(time / 60) % 60;
-      let seconds = time % 60;
-
-      if (hours < 10) {
-        hours = "0" + hours;
-      }
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-
-      clockEl.innerHTML = hours + ":" + minutes + ":" + seconds;
-
-      if (time <= 0) {
-        clearInterval(myInterval);
-      }
-    }, 1000);
-  }
-  startTimer();
+}
+  saveInput();
