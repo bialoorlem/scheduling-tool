6d7da6070982b15ae0278d24e6b2fe601e8d21bb
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-21 15:05:52 -0600

    Added values to HTML

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 //Make sure the current date displays here
-
-
-function showCurrentDate() {
-  let date = document.getElementById("currentDay");
-
-  console.log(date);
-
-  date.innerHTML = moment().format("LL");
-}
-showCurrentDate();
-
-
-//Saving in local storage, received help from Jackson and Bryan
-
-
-
-  document.querySelectorAll(".btn").forEach(function(button){
-    button.addEventListener("click", function(){
-    saveInput();  
-
-    })
-
-  })
-  
-
-  function saveInput() {
-    localStorage.setItem("userInput", userInput);
-  }
-
-let savedValue = localStorage.getItem("userInput");
+storageData = window.localStorage;
+// let savedValue = storageData.getItem("userInput");
 
 let userInput1 = document.getElementById("userInput1");
 
@@ -50,10 +22,90 @@ let userInput9 = document.getElementById("userInput9");
 
 let userInput0 = document.getElementById("userInput0");
 
+let inputs = [
+  userInput0, userInput1,userInput2,userInput3,userInput4,userInput5,userInput6,userInput7,userInput8,userInput9
+];
+
+pullInput();
+
+function showCurrentDate() {
+  let date = document.getElementById("currentDay");
+
+  console.log(date);
+
+  date.innerHTML = moment().format("LL");
+}
+showCurrentDate();
+
+
+//Saving in local storage, received help from Jackson and Bryan
+
+
+
+  document.querySelectorAll(".btn").forEach(function(save){
+    save.addEventListener("click", function(){
+      let inputVal = save.getAttribute("value");
+
+      console.log(inputVal);
+    saveInput(inputVal);  
+
+    })
+
+  })
+  
+
+  function saveInput(x) {
+
+ 
+    let userInput = document.getElementById("userInput" + x).value;
+    console.log(x);
+    let inputName = "userInput" + x;
+    storageData.setItem(inputName, userInput);
+  }
 
 
 function pullInput(){
 
-userInput.innerHTML = savedValue
+  for(i = 0 ; i < 9 ; i ++){
+    let placeHolder = "userInput" + i;
+    console.log(placeHolder);
+    if(storageData.getItem(placeHolder) === null)
+    {
+      continue;
+    }
+    else{
+    inputs[i].innerHTML = storageData.getItem(placeHolder);
+    }
+  }
 
 }
+
+      // function initInput() {
+      //   const buttonEl = document.querySelector("#button");
+      //   buttonEl.addEventListener("click", function() {
+      //     const input = document.getElementById("input").value;
+      //     const dataStr = localStorage.getItem("input") || "[]";
+      //     // if(!dataStr){
+      //     //   dataStr = '[]'
+      //     // }
+      //     const data = JSON.parse(dataStr);
+      //     data.push(input);
+      //     localStorage.setItem("input", JSON.stringify(data));
+      //   });
+      // }
+      // initInput();
+
+      // function initOutput() {
+      //   const outputEl = document.getElementById("output");
+      //   setInterval(function() {
+      //     outputEl.innerHTML = "";
+      //     const dataStr = localStorage.getItem("input") || "[]";
+      //     const data = JSON.parse(dataStr);
+      //     data.forEach(function(output) {
+      //       const h1El = document.createElement("h1");
+      //       h1El.innerHTML = output;
+      //       outputEl.append(h1El);
+      //     });
+      //   }, 100);
+      // }
+      // initOutput();
