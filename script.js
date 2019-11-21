cd2e39986f4d2803f497274f97f611910f18d0c5
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-20 19:58:43 -0600

    Added ids to text areas and took JS from previous homework and inputted into current JS. Will be modifying it to make sure it saves in local storage.

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ function showCurrentDate() {
   date.innerHTML = moment().format("LL");
 }
 showCurrentDate();
+
+function saveInput() {
+
+
+  const clockEl = document.getElementById("clock");
+  let savedValue = localStorage.getItem("time");
+  
+  
+  let myInterval = null;
+
+    if (myInterval) {
+      clearInterval(myInterval);
+      myInterval = null;
+    }
+    myInterval = setInterval(function() {
+      time = time - 1;
+
+      let hours = Math.floor(time / 60 / 60);
+      let minutes = Math.floor(time / 60) % 60;
+      let seconds = time % 60;
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      clockEl.innerHTML = hours + ":" + minutes + ":" + seconds;
+
+      if (time <= 0) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  }
+  startTimer();
